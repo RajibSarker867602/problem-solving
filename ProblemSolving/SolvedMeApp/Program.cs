bbaeb141ec1084a0429b70c018cd1965181e3ae8
Rajib Sarker <45026755+RajibSarker867602@users.noreply.github.com>
2024-08-28 23:15:31 +0600

    solved basic problems and its amazing...

---
+++ ProblemSolving/SolvedMeApp/Program.cs
@@ -0,0 +1,261 @@
+﻿
+#region patterns
+// Problem Statement: Given an integer N, print the following pattern.
+/*
+             * * *
+             * * *
+             * * *
+ */
+
+//Console.Write("Please enter the value of N: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//for (int i = 0; i < n; i++)
+//{
+//    for (int j = 0; j < n; j++)
+//    {
+//        Console.Write("* ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+//Problem Statement: Given an integer N, print the following pattern : 
+
+/* 
+
+*
+* *
+* * *
+* * * *
+* * * * *
+ 
+*/
+
+//Console.Write("Please enter the value of N: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//for(int i = 0; i < n; i++)
+//{
+//    for(int j = 0; j<=i; j++)
+//    {
+//        Console.Write("* ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+// Pattern - 3: Right-Angled Number Pyramid
+
+/* 
+
+1
+1 2 
+1 2 3
+ 
+*/
+
+//Console.Write("Please enter the value of N: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//for (int i = 0; i < n; i++)
+//{
+//    for (int j = 0; j <= i; j++)
+//    {
+//        Console.Write((j + 1) + " ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+
+// Pattern - 4: Right-Angled Number Pyramid - II
+
+/* 
+
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+6 6 6 6 6 6
+ 
+*/
+
+//Console.Write("Please enter the value of N: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//for (int i = 1; i <= n; i++)
+//{
+//    for (int j = 1; j <= i; j++)
+//    {
+//        Console.Write(i + " ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+// Pattern-5: Inverted Right Pyramid
+
+/* 
+
+* * * * * *
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+ 
+*/
+
+//Console.Write("Please enter the value of N: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//for (int i = n; i > 0; i--)
+//{
+//    for (int j = 1; j <= i; j++)
+//    {
+//        Console.Write("* ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+//for (int i = 1; i <= n; i++)
+//{
+//    for (int j = i; j <= n; j++)
+//    {
+//        Console.Write("* ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+// Pattern - 6: Inverted Numbered Right Pyramid
+
+/* 
+
+Input Format: N = 6
+Result:
+1 2 3 4 5 6
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2 
+1
+ 
+*/
+
+//Console.Write("Please enter the value of N: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//for (int i = n; i > 0; i--)
+//{
+//    for (int j = 1; j <= i; j++)
+//    {
+//        Console.Write(j + " ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+// Pattern - 7: Star Pyramid
+
+/* 
+
+Input Format: N = 3
+Result: 
+  *  
+ *** 
+*****
+ 
+Input Format: N = 6
+Result:
+     *     
+    ***    
+   *****   
+  *******  
+ ********* 
+***********
+*/
+
+//Console.Write("Please enter the value of N: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//for (int i = 1; i <= n; i++)
+//{
+//    for (int k = 1; k <= n - i; k++)
+//    {
+//        Console.Write(" ");
+//    }
+
+//    for (int j = 1; j <= (i + (i - 1)); j++)
+//    {
+//        Console.Write("*");
+//    }
+
+//    for (int k = 1; k <= n - i; k++)
+//    {
+//        Console.Write(" ");
+//    }
+
+//    Console.WriteLine();
+//}
+
+
+
+// Pattern - 8: Inverted Star Pyramid
+
+/* 
+
+Input Format: N = 3
+Result: 
+*****  
+ ***
+  *   
+  
+Input Format: N = 6
+Result:     
+***********
+ *********
+  *******
+   ***** 
+    ***    
+     *
+     
+***********
+ *********
+  *******
+   *****
+    ***
+     *
+*/
+
+Console.Write("Please enter the value of N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = n; i > 0; i--)
+{
+    for (int k = 1; k <= n - i; k++)
+    {
+        Console.Write(" ");
+    }
+
+    for (int j = 1; j <= (i + (i - 1)); j++)
+    {
+        Console.Write("*");
+    }
+
+    for (int k = 1; k <= n - i; k++)
+    {
+        Console.Write(" ");
+    }
+
+    Console.WriteLine();
+}
+
+#endregion
